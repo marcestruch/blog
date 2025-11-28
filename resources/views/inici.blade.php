9726731035a19a90504d1b080dce0743c3071b54
Marc <marcstruchmalonda@gmail.com>
2025-11-28 18:55:32 +0100

    Cambios en el texto de inicio

--- resources/views/inici.blade.php
+++ resources/views/inici.blade.php
@@ -6,7 +6,7 @@
 <div class="p-5 mb-4 bg-light rounded-3">
     <div class="container-fluid py-5">
         <h1 class="display-5 fw-bold">Benvingut al meu Portfoli</h1>
-        <p class="col-md-8 fs-4">Hola! Sóc Marc, i aquest és el meu blog personal on comparteixo els meus projectes de desenvolupament web, experiments amb Laravel, i molt més.</p>
+        <p class="col-md-8 fs-4">Hola! Sóc Marc, i aquest és el meu blog personal on compartixc els meus projectes de desenvolupament web, experiments amb Laravel, i molt més.</p>
         <a href="{{ route('posts_llistat') }}" class="btn btn-primary btn-lg" type="button">Veure Projectes</a>
         <a href="https://github.com/marcestruch" target="_blank" class="btn btn-dark btn-lg" type="button"><i class="bi bi-github"></i> El meu GitHub</a>
     </div>
@@ -16,7 +16,7 @@
     <div class="col-md-6">
         <div class="h-100 p-5 text-white bg-dark rounded-3">
             <h2>Sobre mi</h2>
-            <p>M'apassiona la tecnologia i el desenvolupament de programari. Estic constantment aprenent noves eines i frameworks per millorar les meves habilitats.</p>
+            <p>M'apassiona la tecnologia i el desenvolupament. Estic constantment aprenent noves eines i frameworks per millorar les meves habilitats.</p>
             <a href="{{ route('SobreMi') }}" class="btn btn-primary btn-lg" type="button">Sobre mi</a>
         </div>
     </div>

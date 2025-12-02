ba377b2d2aebd7ebf11f242f777d27df7421d543
Marc <marcstruchmalonda@gmail.com>
2025-12-02 18:42:20 +0100

    feat: Implement posts table, simplify user authentication schema, and remove default cache and job migrations.

--- database/migrations/2025_12_02_170036_crear_taula_posts.php
+++ database/migrations/2025_12_02_170036_crear_taula_posts.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('login')->unique();
-            $table->string('password');
+            $table->string('titol');
+            $table->text('contingut');
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
